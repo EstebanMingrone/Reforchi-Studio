6c601df95172ea22dfd2f3fe642f3c14a83b9273
Esteban Mingrone <mingrone.fe@gmail.com>
2024-02-17 05:02:02 -0300

    Reforchi Studio v1.0

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+function sendEmail(params) {
+    console.log(params)
+    
+    const name = document.getElementById('name').value
+    const surname = document.getElementById('surname').value
+    const email = document.getElementById('email').value
+    const tel = document.getElementById('tel').value
+    const mensaje = document.getElementById('mensaje').value
+
+
+    
+    var link = "mailto:" + escape(email)
+         + "?subject=" + escape("Contacto Reforchi Studio")
+         + "&body=" + escape(mensaje);
+
+     window.location.href = link;
+}
