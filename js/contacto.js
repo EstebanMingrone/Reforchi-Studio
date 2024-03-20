c91280f9ddc32dc6760a851076d81a0cee39402c
Esteban Mingrone <mingrone.fe@gmail.com>
2024-03-20 13:01:07 -0300

    Reforchi v1.6 (Sitemap + Fixed Form)

--- js/contacto.js
+++ js/contacto.js
@@ -1,17 +1,43 @@
-function enviarCorreo() {
-    var nombre = document.getElementById('nombre').value;
-    var email = document.getElementById('email').value;
-    var mensaje = document.getElementById('mensaje').value;
+const nombre = document.getElementById('nombre').value;
+const email = document.getElementById('email').value;
+const tel = document.getElementById('tel').value;
+const mensaje = document.getElementById('mensaje').value;
+const submit = document.getElementsByClassName('form-contact')[0];
+
+submit.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let ebody= `
+    <b>Nombre: </b>${nombre.value}    
+    <br>
+    <b>Email: </b>${email.value}
+    <br>
+    <b>Teléfono: </b>${tel.value}
+    <br>
+    <b>Mensaje: </b>${mensaje.value}
+    `;
 
     Email.send({
-      SecureToken: "d09983ec-053a-4292-a805-a83144c32ada",
-      To: 'reforchistudio@gmail.com',
-      From: email,
-      Subject: "Mensaje de contacto de " + nombre,
-      Body: mensaje
+        SecureToken: "c42b35ec-7325-4e73-bbbe-a199248ac02a",
+        To: 'reforchistudio@gmail.com',
+        From: 'reforchistudio@gmail.com',
+        Subject: "Email de " + email.value,
+        Body: ebody
     }).then(
-      Swal.fire({
-        template: "#my-template"
-    })
-    );
-}
+        function(response) {
+            if (response === 'OK') {
+                Swal.fire({
+                    title: 'Correo enviado',
+                    text: 'Tu mensaje ha sido enviado correctamente.',
+                    icon: 'success'
+                });
+            } else {
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Hubo un problema al enviar el correo. Por favor, inténtalo de nuevo más tarde.',
+                    icon: 'error'
+                });
+            }
+        }
+    )
+});
+
