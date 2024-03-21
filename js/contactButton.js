a0f1c49909348428c722980c52e7f30b735ed1a1
Esteban Mingrone <mingrone.fe@gmail.com>
2024-03-21 13:52:28 -0300

    Reforchi v1.7.2 (Contact Btn Test)

--- js/contactButton.js
+++ js/contactButton.js
@@ -1,21 +1,47 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault(); // Prevenir el envío del formulario por defecto
   
-    var nombre = document.getElementById('nombre').value;
-    var email = document.getElementById('email').value;
-    var mensaje = document.getElementById('mensaje').value;
-  
-    Email.send({
-      SecureToken: "d09983ec-053a-4292-a805-a83144c32ada",
+  const nombre = document.getElementById('nombre').value;
+  const email = document.getElementById('email').value;
+  const tel = document.getElementById('tel').value;
+  const mensaje = document.getElementById('mensaje').value;
+
+  let ebody = `
+      <b>Nombre: </b>${nombre}<br>
+      <b>Email: </b>${email}<br>
+      <b>Teléfono: </b>${tel}<br>
+      <b>Mensaje: </b>${mensaje}
+  `;
+
+  Email.send({
+      SecureToken: "3362a2b6-a7c9-4c35-bf9c-ae8dad60d400",
       To: 'reforchistudio@gmail.com',
       From: email,
-      Subject: "Mensaje de contacto de " + nombre,
-      Body: mensaje
-    }).then(
-      message => {
-        alert("Correo enviado exitosamente");
-        $('#contactModal').modal('hide'); // Cerrar el modal de Bootstrap después de enviar el correo
+      Subject: "Correo de contacto de " + nombre,
+      Body: ebody
+  }).then(
+      function(response) {
+          if (response === 'OK' || (response && response.status === 200)) { // Verificar si la solicitud fue exitosa
+              Swal.fire({
+                  title: 'Correo enviado',
+                  text: 'Tu mensaje ha sido enviado correctamente.',
+                  icon: 'success'
+              });
+          } else {
+              Swal.fire({
+                  title: 'Error',
+                  text: 'Hubo un problema al enviar el correo. Por favor, inténtalo de nuevo más tarde.',
+                  icon: 'error'
+              });
+          }
       }
-    );
+  ).catch(function(error) {
+      console.error('Error al enviar el correo:', error);
+      Swal.fire({
+          title: 'Error',
+          text: 'Hubo un problema al enviar el correo. Por favor, inténtalo de nuevo más tarde.',
+          icon: 'error'
+      });
   });
+});
   
