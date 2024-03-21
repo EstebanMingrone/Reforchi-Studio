05509cdc6a110e2ec57f158e06abba61615f0323
Esteban Mingrone <mingrone.fe@gmail.com>
2024-03-21 13:27:42 -0300

    Reforchi v1.7 (SMTP Fix)

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,59 @@
-const nombre = document.getElementById('nombre').value;
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevenir el envío del formulario por defecto
+    
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const tel = document.getElementById('tel').value;
+    const mensaje = document.getElementById('mensaje').value;
+
+    let ebody = `
+        <b>Nombre: </b>${nombre}<br>
+        <b>Email: </b>${email}<br>
+        <b>Teléfono: </b>${tel}<br>
+        <b>Mensaje: </b>${mensaje}
+    `;
+
+    Email.send({
+        SecureToken: "3362a2b6-a7c9-4c35-bf9c-ae8dad60d400",
+        To: 'reforchistudio@gmail.com',
+        From: 'reforchistudio@gmail.com',
+        Subject: "Correo de contacto de " + nombre,
+        Body: ebody
+    }).then(
+        function(response) {
+            if (response === 'OK' || (response && response.status === 200)) { // Verificar si la solicitud fue exitosa
+                Swal.fire({
+                    title: 'Correo enviado',
+                    text: 'Tu mensaje ha sido enviado correctamente.',
+                    icon: 'success'
+                });
+            } else {
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Hubo un problema al enviar el correo. Por favor, inténtalo de nuevo más tarde.',
+                    icon: 'error'
+                });
+            }
+        }
+    ).catch(function(error) {
+        console.error('Error al enviar el correo:', error);
+        Swal.fire({
+            title: 'Error',
+            text: 'Hubo un problema al enviar el correo. Por favor, inténtalo de nuevo más tarde.',
+            icon: 'error'
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+/*const nombre = document.getElementById('nombre').value;
 const email = document.getElementById('email').value;
 const tel = document.getElementById('tel').value;
 const mensaje = document.getElementById('mensaje').value;
@@ -17,7 +72,7 @@ submit.addEventListener('submit', (e)=>{
     `;
 
     Email.send({
-        SecureToken: "c42b35ec-7325-4e73-bbbe-a199248ac02a",
+        SecureToken: "91cc0533-7c05-4508-9b63-3275618238a7",
         To: 'reforchistudio@gmail.com',
         From: 'reforchistudio@gmail.com',
         Subject: "Email de " + email.value,
@@ -39,5 +94,5 @@ submit.addEventListener('submit', (e)=>{
             }
         }
     )
-});
+});*/
 
