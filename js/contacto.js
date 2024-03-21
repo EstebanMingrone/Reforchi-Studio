beac1d7008aa0b0f4acfcda2ab4789590ed072de
Esteban Mingrone <mingrone.fe@gmail.com>
2024-03-21 16:44:24 -0300

    Reforchi v1.8 (BTN Fixed & htaccess file)

--- js/contacto.js
+++ js/contacto.js
@@ -14,7 +14,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     `;
 
     Email.send({
-        SecureToken: "3362a2b6-a7c9-4c35-bf9c-ae8dad60d400",
+        SecureToken: "7a237082-4f58-4a79-b73f-785a27e313c0",
         To: 'reforchistudio@gmail.com',
         From: email,
         Subject: "Correo de contacto de " + nombre,
@@ -45,55 +45,3 @@ document.getElementById('contact-form').addEventListener('submit', function(even
         });
     });
 });
-
-
-
-
-
-
-
-
-
-/*const nombre = document.getElementById('nombre').value;
-const email = document.getElementById('email').value;
-const tel = document.getElementById('tel').value;
-const mensaje = document.getElementById('mensaje').value;
-const submit = document.getElementsByClassName('form-contact')[0];
-
-submit.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    let ebody= `
-    <b>Nombre: </b>${nombre.value}    
-    <br>
-    <b>Email: </b>${email.value}
-    <br>
-    <b>Teléfono: </b>${tel.value}
-    <br>
-    <b>Mensaje: </b>${mensaje.value}
-    `;
-
-    Email.send({
-        SecureToken: "91cc0533-7c05-4508-9b63-3275618238a7",
-        To: 'reforchistudio@gmail.com',
-        From: 'reforchistudio@gmail.com',
-        Subject: "Email de " + email.value,
-        Body: ebody
-    }).then(
-        function(response) {
-            if (response === 'OK') {
-                Swal.fire({
-                    title: 'Correo enviado',
-                    text: 'Tu mensaje ha sido enviado correctamente.',
-                    icon: 'success'
-                });
-            } else {
-                Swal.fire({
-                    title: 'Error',
-                    text: 'Hubo un problema al enviar el correo. Por favor, inténtalo de nuevo más tarde.',
-                    icon: 'error'
-                });
-            }
-        }
-    )
-});*/
-
